90515bb4258dc40d7fc07f1bf86f944a5e35512c
Krishna Nagella <krishna.ror7@gmail.com>
2016-06-18 23:46:51 -0400

    Changes in deploying

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,15 +2,15 @@
 lock '3.5.0'
 
 set :application, 'vchat'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:nagella477/Vchat.git'
 set :branch, :master
-set :deploy_to, '/home/deploy/contactbook'
+set :deploy_to, '/home/deploy/vchat'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
-set :rvm_ruby_version, 'jruby-1.7.19' # Edit this if you are using MRI Ruby
+set :rvm_ruby_version, 'ruby 2.2.4p230' # Edit this if you are using MRI Ruby
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
